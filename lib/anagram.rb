0f8fe2d8072871d8553797aeb9f66c2f36644725
norman lewis <takialog@gmail.com>
2018-03-03 01:19:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :listen
+
+  def initialize(listen)
+    @listen = listen
+  end
+
+  def match(words)
+    pattern = @listen.split("").sort
+    words.select do |w|
+      w.split("").sort == pattern
+    end
+  end
+
+end
